36b8fa1897f8d0612c104ba06093e0209ccf7bc2
SkwynethPaltrow <will.sklenars@gmail.com>
2015-08-05 18:40:33 +1200

    initial cm

---
+++ js/application.js
@@ -0,0 +1,44 @@
+var timer = null
+var mesh;
+var balls = []
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 1,10000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// setup orbit controls
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.target = new THREE.Vector3(0, 0, 0);
+
+
+for (var i = 0; i < 1; i++) {
+
+  var geometry = new THREE.SphereGeometry(150, 150, 150);
+  var material = new THREE.MeshNormalMaterial({shading: THREE.FlatShading});
+  var mesh = new THREE.Mesh(geometry, material);
+  balls.push(mesh)
+  scene.add( mesh );
+
+};
+
+camera.position.z = 2000;
+
+
+function render() {
+  requestAnimationFrame(render);
+  timer = Date.now()/1000 // increments 1 per second
+  // console.log(timer)
+
+
+renderer.render(scene, camera);
+}
+
+render();
+
+
+
+
+
+
