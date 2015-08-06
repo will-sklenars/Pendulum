b0e92a469cd5d280ab3caf243f8fd9108237ee73
SkwynethPaltrow <will.sklenars@gmail.com>
2015-08-06 22:35:49 +1200

    fuuuuuuuk

--- application.js
+++ application.js
@@ -6,9 +6,9 @@ var helpers = require('./js/helpers');
 var Pendulum = require('./js/pendulum');
 
 var mesh;
-var balls = [];
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 1,10000);
+var camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight, 1,90000);
+camera.position.z = 2000;
 
 // Add axis helper to show axis in x (red) and y (green) z (blue) direction, remove later
 var axisHelper = new THREE.AxisHelper( 10000 );
@@ -23,26 +23,33 @@ var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target = new THREE.Vector3(0, 0, 0);
 
 
-for (var i = 0; i < 1; i++) {
+// for (var i = 0; i < 1; i++) {
 
-  var geometry = new THREE.SphereGeometry(150, 150, 150);
-  var material = new THREE.MeshNormalMaterial({shading: THREE.FlatShading});
-  var mesh = new THREE.Mesh(geometry, material);
-  balls.push(mesh)
-  scene.add( mesh );
+//   var geometry = new THREE.SphereGeometry(150, 150, 150)
+//   var material = new THREE.MeshNormalMaterial({shading: THREE.FlatShading})
+//   var mesh = new THREE.Mesh(geometry, material)
+//   balls.push( mesh )
+//   scene.add( mesh )
 
 
+// };
+
+
+var pendulums = []
+for (var i = 0; i < 15; i++) {
+  pendulums.push(
+    new Pendulum({
+      length: (i + 1),
+      z: 400*(i + 1)
+    })
+  )
 };
 
-camera.position.z = 2000;
 
-var pendulums = [
-  new Pendulum({
-    length: 1,
-    z: 0,
 
-  })
-]
+pendulums.forEach(function (pendulum) {
+  pendulum.addToScene(scene)
+})
 
 function render() {
   requestAnimationFrame(render);
--- js/pendulum.js
+++ js/pendulum.js
@@ -1,3 +1,5 @@
+var THREE = require('./three.min.js');
+
 module.exports = Pendulum
 
 function Pendulum (opts) {
@@ -12,12 +14,25 @@ function Pendulum (opts) {
   // x and y vary with t
   this.x = opts.x || 0
   this.y = opts.y || 0
+  this.mesh = createMesh()
+  this.mesh.position.z = this.z
+}
+
+var createMesh = function() {
+  var geometry = new THREE.SphereGeometry(150, 150, 150)
+  var material = new THREE.MeshNormalMaterial({shading: THREE.FlatShading})
+  return new THREE.Mesh(geometry, material)
+};
+
+Pendulum.prototype.update = function update (t) {
+  var dt = t - this.initialTime
+  // compute theta, z, and y based on t
+  this.mesh.position.x = Math.sin( Date.now() * 0.001 * this.length ) * 500;
+  this.mesh.position.y = Math.sin( Date.now() * 0.001 * this.length/4 ) * 500;
+  // console.log(this)
 
 }
 
-Pendulum.prototype.update = update
-
-function update (t) {
-  var dt = t - this.initialTime
-  // compute theta, z, and y based on t
+Pendulum.prototype.addToScene = function (scene) {
+  scene.add(this.mesh)
 }
